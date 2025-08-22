ec21a643b5bef1c08eb2fe55bf41fda02f07def0
SrinivasPabballa <srinivassunny64@gmail.com>
2025-08-22 15:53:12 +0530

    DB

--- Backend.sh
+++ Backend.sh
@@ -79,7 +79,7 @@ VALIDATE  $? "Enabling backend"
 dnf install mysql -y &>>$LOGFILE 
 VALIDATE  $? "Installing Mysql"
 
-mysql -h db.daws-78s.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE  
+mysql -h 54.158.63.80 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE  
 VALIDATE  $? "Schema loading" 
 
 systemctl restart backend &>>$LOGFILE 
